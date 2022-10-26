17c58e0499cc44983e590f1ce1cc20be8e2d4c40
Jack <Jackferson15@gmail.com>
2022-10-26 14:10:53 -0300

    update config

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const MONGODB_URI = process.env.MONGODB_URI;
-export const PORT = process.env.PORT
+export const PORT = process.env.PORT || 3000
